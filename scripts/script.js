b3471364828530a63b823d38d1b887844d17bf30
Mizanur_Rahman <mizanurictiu@gmail.com>
2024-02-13 16:47:20 +0600

    add Triangle area

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function triangleCalculateArea() {
+  const triangleBaseInput = document.getElementById("triangle-base");
+  const triangleBaseText = triangleBaseInput.value;
+  const base = parseFloat(triangleBaseText);
+
+  const triangleHeightInput = document.getElementById("triangle-height");
+  const triangleHeightText = triangleHeightInput.value;
+  const height = parseFloat(triangleHeightText);
+  
+  const area = 0.5 * base * height;
+  const areaCalculate = document.getElementById("triangleArea");
+  areaCalculate.innerText = area;
+
+}
